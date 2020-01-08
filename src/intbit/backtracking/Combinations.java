142bb901ef83b1506184bbcda11306c4291ac88e
praveen singh <praveensingh0107@gmail.com>
2020-01-08 21:44:12 +0530

    backtracking/Combinations.java

--- src/intbit/backtracking/Combinations.java
+++ src/intbit/backtracking/Combinations.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 
 /**
  * Given two integers n and k, return all possible combinations of k numbers out of 1 2 3 ... n.
- *
+ * <p>
  * Make sure the combinations are sorted.
- *
+ * <p>
  * To elaborate,
- *
+ * <p>
  * Within every entry, elements should be sorted. [1, 4] is a valid entry while [4, 1] is not.
  * Entries should be sorted within themselves.
  * Example :
  * If n = 4 and k = 2, a solution is:
- *
+ * <p>
  * [
- *   [1,2],
- *   [1,3],
- *   [1,4],
- *   [2,3],
- *   [2,4],
- *   [3,4],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * [2,3],
+ * [2,4],
+ * [3,4],
  * ]
- * */
+ */
 public class Combinations {
     public ArrayList<ArrayList<Integer>> combine(int n, int k) {
         if (n < k || n < 0 || k < 0) {
@@ -33,12 +33,13 @@ public class Combinations {
         return res;
     }
 
-    private void combine(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> list, int index, int n, int k) {
+    private void combine(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> list, int index,
+            int n, int k) {
         if (list.size() == k) {
             res.add((ArrayList<Integer>) list.clone());
         } else {
             for (int i = index; i < n; i++) {
-                list.add(i+1);
+                list.add(i + 1);
                 combine(res, list, i + 1, n, k);
                 list.remove(list.size() - 1);
             }
@@ -47,9 +48,9 @@ public class Combinations {
 
     public static void main(String[] args) {
         Combinations obj = new Combinations();
-        System.out.println(obj.combine(4,2));
-        System.out.println(obj.combine(4,3));
-        System.out.println(obj.combine(4,1));
-        System.out.println(obj.combine(4,0));
+        System.out.println(obj.combine(4, 2));
+        System.out.println(obj.combine(4, 3));
+        System.out.println(obj.combine(4, 1));
+        System.out.println(obj.combine(4, 0));
     }
 }
