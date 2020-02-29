7fa1c376a179d3fc19e02008444839cdb0515b0f
praveen singh <praveensingh0107@gmail.com>
2020-02-29 14:36:21 +0530

    Substring Concatenation example

--- src/intbit/hashing/SubstringConcatenation.java
+++ src/intbit/hashing/SubstringConcatenation.java
@@ -7,11 +7,11 @@ import java.util.Map;
 
 /**
  * You are given a string, S, and a list of words, L, that are all of the same length.
- *
+ * <p>
  * Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
- *
+ * <p>
  * Example :
- *
+ * <p>
  * S: "barfoothefoobarman"
  * L: ["foo", "bar"]
  * You should return the indices: [0,9].
@@ -27,7 +27,7 @@ public class SubstringConcatenation {
         if (windowSize > A.length())
             return result;
         Map<String, Integer> map = new HashMap<>(); // to count the number of each element in B
-        B.stream().forEach(i-> map.merge(i, 1, Integer::sum));
+        B.stream().forEach(i -> map.merge(i, 1, Integer::sum));
         int hashSumOfB = B.stream().map(this::hash).reduce(0, Integer::sum);
         int startIndex = 0, endIndex = windowSize;
         int hashOfSubstring = hash(A.substring(startIndex, endIndex));
@@ -64,28 +64,31 @@ public class SubstringConcatenation {
         }
         return sum;
     }
+
     public int hash(char c) {
         return c - 'a';
     }
+
     /**
      * A : "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
      * B : [ "aaa", "aaa", "aaa", "aaa", "aaa" ]
+     *
      * @param args
      */
 
     public static void main(String[] args) {
         SubstringConcatenation obj = new SubstringConcatenation();
-        String a  = "barfoothefoobarman";
+        String a = "barfoothefoobarman";
         List<String> b = List.of("foo", "bar");
         System.out.println(obj.findSubstring(a, b));
-        String A  = "abbaccaaabcabbbccbabbccabbacabcacbbaabbbbbaaabaccaacbccabcbababbbabccabacbbcabbaacaccccbaabcabaabaaaabcaabcacabaa";
+        String A = "abbaccaaabcabbbccbabbccabbacabcacbbaabbbbbaaabaccaacbccabcbababbbabccabacbbcabbaacaccccbaabcabaabaaaabcaabcacabaa";
         List<String> B = List.of("cac", "aaa", "aba", "aab", "abc");
         System.out.println(obj.findSubstring(A, B));
-        String A1  = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        List<String> B1 = List.of("aaa", "aaa", "aaa", "aaa", "aaa" );
+        String A1 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        List<String> B1 = List.of("aaa", "aaa", "aaa", "aaa", "aaa");
         System.out.println(obj.findSubstring(A1, B1));
-        String A2  = "bcabbcaabbccacacbabccacaababcbb";
-        List<String> B2 = List.of("c", "b", "a", "c", "a", "a", "a", "b", "c" );
+        String A2 = "bcabbcaabbccacacbabccacaababcbb";
+        List<String> B2 = List.of("c", "b", "a", "c", "a", "a", "a", "b", "c");
         System.out.println(obj.findSubstring(A2, B2));
     }
 }
